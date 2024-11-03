c9ef40c6ffcfbe56e1b447e76ad6bc8a37e59d1f
Chris Cashman <chris.cashman91@gmail.com>
2024-11-03 22:34:54 +0000

    Replaced red blobs with asteroids

--- SpaceMiner.py
+++ SpaceMiner.py
@@ -129,9 +129,15 @@ class Player(pygame.sprite.Sprite):
 class Food(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((15, 15), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, Colour.RED, (7, 7), 7)
+
+        asteroid_image = pygame.image.load("assets/asteroid.png").convert_alpha()
+        self.image = pygame.transform.scale(asteroid_image, (7, 7))
+        rotation_angle = random.choice([0, 90, 180, 270])
+        self.image = pygame.transform.rotate(asteroid_image, rotation_angle)
+        
+        # Set the position of the asteroid
         self.rect = self.image.get_rect(center=(random.randint(10, WIDTH - 10), random.randint(10, HEIGHT - 10)))
+
 
 # Sprite Groups
 all_sprites = pygame.sprite.Group()
